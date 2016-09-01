e9a204718766fe1c6887cdc32dad20ce37300a2a
Adam Kirk <atomkirk@gmail.com>
2016-09-01 06:43:24 -0600

    marking private methods with _underscore

--- flask.js
+++ flask.js
@@ -1,8 +1,8 @@
 class Flask {
 
   constructor(canvas) {
-    this.canvas = canvas;
-    this.setup();
+    this._canvas = canvas;
+    this._setup();
 
     // public
     this.zoomSpeed = 1.01;
@@ -12,92 +12,90 @@ class Flask {
     this.maxScale = 2;
 
     // private
-    this.ctx = canvas.getContext('2d');
-    this.dragStart;
-    this.canvas;
-    this.ctx;
-    this.svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
-    this.transform = this.svg.createSVGMatrix();
-    this.tileCache = {};
+    this._ctx = canvas.getContext('2d');
+    this._dragStart;
+    this._svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+    this._transform = this._svg.createSVGMatrix();
+    this._tileCache = {};
 
-    var minScaleX = this.canvas.width / this.width;
-    var minScaleY = this.canvas.height / this.height;
-    this.minScale = Math.max(minScaleX, minScaleY);
+    var minScaleX = this._canvas.width / this.width;
+    var minScaleY = this._canvas.height / this.height;
+    this._minScale = Math.max(minScaleX, minScaleY);
 
-    this.annotations = [];
-    this.currentAnnotation = [];
-    this.currentShape;
-    this.state = {
+    this._annotations = [];
+    this._currentAnnotation = [];
+    this._currentShape;
+    this._state = {
       mouse: 'up', // 'down'
       tool: 'none', // 'arrow', 'line', 'circle', 'rectangle', 'freehand'
       dragging: false,
       drawing: false
     }
 
-    this.redraw();
+    this._redraw();
   }
 
-  setup() {
-    this.canvas.addEventListener('mousedown', this.mouseDown.bind(this), false);
-    this.canvas.addEventListener('mousemove', this.mouseMove.bind(this), false);
-    this.canvas.addEventListener('mouseup', this.mouseUp.bind(this), false);
-    this.canvas.addEventListener('DOMMouseScroll', this.updateZoom.bind(this), false);
-    this.canvas.addEventListener('mousewheel', this.updateZoom.bind(this), false);
-    window.addEventListener('keyup', this.keyUp.bind(this), false);
+  _setup() {
+    this._canvas.addEventListener('mousedown', this._mouseDown.bind(this), false);
+    this._canvas.addEventListener('mousemove', this._mouseMove.bind(this), false);
+    this._canvas.addEventListener('mouseup', this._mouseUp.bind(this), false);
+    this._canvas.addEventListener('DOMMouseScroll', this._updateZoom.bind(this), false);
+    this._canvas.addEventListener('mousewheel', this._updateZoom.bind(this), false);
+    window.addEventListener('keyup', this._keyUp.bind(this), false);
   }
 
-  updateZoom(e) {
+  _updateZoom(e) {
     var delta = e.wheelDelta ? e.wheelDelta / 40 : (e.detail ? -e.detail : 0);
     if (delta) {
-      var pt = this.eventPoint(e);
+      var pt = this._eventPoint(e);
 
       // update any shape that is currently being drawn
-      if (this.currentShape) {
-        this.currentShape.geometry.p2 = pt;
+      if (this._currentShape) {
+        this._currentShape.geometry.p2 = pt;
       }
 
       var factor = Math.pow(this.zoomSpeed, delta);
-      var scale = this.clamp(this.transform.a * factor, this.minScale, this.maxScale);
+      var scale = Flask.clamp(this._transform.a * factor, this._minScale, this.maxScale);
 
       // move point of mouse to center
-      this.transform = this.transform.translate(pt.x, pt.y);
+      this._transform = this._transform.translate(pt.x, pt.y);
       // scale
-      this.transform.a = this.transform.d = scale;
+      this._transform.a = this._transform.d = scale;
       // move back
-      this.transform = this.transform.translate(-pt.x, -pt.y);
+      this._transform = this._transform.translate(-pt.x, -pt.y);
 
-      this.clampToBounds();
-      this.updateTransform();
+      this._clampToBounds();
+      this._updateTransform();
     }
     return e.preventDefault() && false;
   }
 
-  mouseDown(e) {
-    this.state.mouse = 'down';
+  _mouseDown(e) {
+    this._state.mouse = 'down';
     document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
-    this.dragStart = this.eventPoint(e);
+    this._dragStart = this._eventPoint(e);
   }
 
-  mouseUp(e) {
-    this.state.mouse = 'up';
+  _mouseUp(e) {
+    this._state.mouse = 'up';
     // draw
-    if (this.state.dragging) {
-      this.state.dragging = false;
+    if (this._state.dragging) {
+      this._state.dragging = false;
     }
-    else if (!this.state.drawing && this.state.tool !== 'none') {
-      this.state.drawing = true;
-      var pt = this.eventPoint(e);
-      if (this.state.tool === 'arrow' || this.state.tool === 'line' || this.state.tool === 'rectangle') {
-        this.currentShape = {
-          kind: this.state.tool,
+    else if (!this._state.drawing && this._state.tool !== 'none') {
+      this._state.drawing = true;
+      var pt = this._eventPoint(e);
+      if (this._state.tool === 'arrow' || this._state.tool === 'line' || this._state.tool === 'rectangle') {
+        this._currentShape = {
+          kind: this._state.tool,
           geometry: {
             p1: pt,
             p2: pt
           }
         }
       }
-      else if (this.state.tool === 'circle') {
-        this.currentShape = {
+      else if (this._state.tool === 'circle') {
+        this._currentShape = {
           kind: 'circle',
           geometry: {
             center: pt,
@@ -106,93 +104,93 @@ class Flask {
         }
       }
     }
-    else if (this.state.drawing) {
-      this.state.drawing = false;
-      this.currentAnnotation.push(this.currentShape);
-      this.currentShape = null;
+    else if (this._state.drawing) {
+      this._state.drawing = false;
+      this._currentAnnotation.push(this._currentShape);
+      this._currentShape = null;
     }
   }
 
-  mouseMove(e) {
-    var pt = this.eventPoint(e);
-    if (this.state.mouse === 'down' && !this.state.dragging) {
-      this.state.dragging = true;
+  _mouseMove(e) {
+    var pt = this._eventPoint(e);
+    if (this._state.mouse === 'down' && !this._state.dragging) {
+      this._state.dragging = true;
     }
-    if (this.state.dragging) {
-      var dx = (pt.x - this.dragStart.x);
-      var dy = (pt.y - this.dragStart.y);
-      this.transform = this.transform.translate(dx, dy);
-      this.clampToBounds();
-      this.updateTransform();
+    if (this._state.dragging) {
+      var dx = (pt.x - this._dragStart.x);
+      var dy = (pt.y - this._dragStart.y);
+      this._transform = this._transform.translate(dx, dy);
+      this._clampToBounds();
+      this._updateTransform();
     }
-    if (this.state.drawing) {
-      if (this.state.tool === 'arrow' || this.state.tool === 'line' || this.state.tool === 'rectangle') {
-        this.currentShape.geometry.p2 = pt;
-        this.redraw();
+    if (this._state.drawing) {
+      if (this._state.tool === 'arrow' || this._state.tool === 'line' || this._state.tool === 'rectangle') {
+        this._currentShape.geometry.p2 = pt;
+        this._redraw();
       }
-      else if (this.state.tool === 'circle') {
-        var c = this.currentShape.geometry.center;
-        this.currentShape.geometry.radius = Math.sqrt(Math.pow(pt.x - c.x, 2) + Math.pow(pt.y - c.y, 2));
-        this.redraw();
+      else if (this._state.tool === 'circle') {
+        var c = this._currentShape.geometry.center;
+        this._currentShape.geometry.radius = Math.sqrt(Math.pow(pt.x - c.x, 2) + Math.pow(pt.y - c.y, 2));
+        this._redraw();
       }
     }
   }
 
-  keyUp(e) {
+  _keyUp(e) {
     // cancel drawing
-    if (this.state.drawing && e.which === 27) {
-      this.state.drawing = false;
-      this.currentShape = null;
-      this.redraw();
+    if (this._state.drawing && e.which === 27) {
+      this._state.drawing = false;
+      this._currentShape = null;
+      this._redraw();
     }
     // arrow
     else if (e.which === 65) {
-      this.state.tool = 'arrow';
+      this._state.tool = 'arrow';
     }
     // line
     else if (e.which === 76) {
-      this.state.tool = 'line';
+      this._state.tool = 'line';
     }
     // circle
     else if (e.which === 67) {
-      this.state.tool = 'circle';
+      this._state.tool = 'circle';
     }
     // rectangle
     else if (e.which === 82) {
-      this.state.tool = 'rectangle';
+      this._state.tool = 'rectangle';
     }
   }
 
   // transform the point from page space to canvas space
-  transformedPoint(x, y) {
-    var pt  = this.svg.createSVGPoint();
+  _transformedPoint(x, y) {
+    var pt  = this._svg.createSVGPoint();
     pt.x = x;
     pt.y = y;
-    return pt.matrixTransform(this.transform.inverse());
+    return pt.matrixTransform(this._transform.inverse());
   }
 
   // set the transform on the context
-  updateTransform() {
-    var m = this.transform;
-    this.ctx.setTransform(m.a, m.b, m.c, m.d, m.e, m.f);
-    this.redraw();
+  _updateTransform() {
+    var m = this._transform;
+    this._ctx.setTransform(m.a, m.b, m.c, m.d, m.e, m.f);
+    this._redraw();
   }
 
   // clear the canvas and draw the tiles
-  redraw() {
+  _redraw() {
     // Clear the entire canvas
-    var p1 = this.transformedPoint(0, 0);
-    var p2 = this.transformedPoint(this.canvas.width, this.canvas.height);
+    var p1 = this._transformedPoint(0, 0);
+    var p2 = this._transformedPoint(this._canvas.width, this._canvas.height);
     var rect = {
       x: p1.x,
       y: p1.y,
       width: p2.x - p1.x,
       height: p2.y - p1.y
     }
-    this.ctx.clearRect(rect.x, rect.y, rect.width, rect.height);
-    var mins = Math.log(this.minScale) / Math.LN2;
+    this._ctx.clearRect(rect.x, rect.y, rect.width, rect.height);
+    var mins = Math.log(this._minScale) / Math.LN2;
     var maxs = Math.log(this.maxScale) / Math.LN2;
-    var s = Math.log(this.transform.a) / Math.LN2;
+    var s = Math.log(this._transform.a) / Math.LN2;
     var zoomPercent = (s - mins) / (maxs - mins);
     var zoomLevel = Math.max(Math.ceil(zoomPercent * this.maxZoomLevel), 1);
     var numTiles = Math.pow(2, zoomLevel);
@@ -206,108 +204,108 @@ class Flask {
           width: Math.floor(tileWidth),
           height: Math.floor(tileHeight)
         }
-        if (this.intersectRect(rect, tile)) {
-          var image = this.tileImage(zoomLevel, j, k);
+        if (this._intersectRect(rect, tile)) {
+          var image = this._tileImage(zoomLevel, j, k);
           if (image.complete && image.naturalHeight !== 0) {
-            this.ctx.drawImage(image, tile.x, tile.y, tile.width, tile.height);
+            this._ctx.drawImage(image, tile.x, tile.y, tile.width, tile.height);
           }
         }
       }
     }
 
     // set how annotations should look
-    this.ctx.fillStyle = '#FF0000';
-    this.ctx.strokeStyle = '#FF0000';
-    this.ctx.lineWidth = 10;
+    this._ctx.fillStyle = '#FF0000';
+    this._ctx.strokeStyle = '#FF0000';
+    this._ctx.lineWidth = 10;
 
     // draw the current annotation
-    this.currentAnnotation.concat(this.currentShape || []).forEach(annotation => {
+    this._currentAnnotation.concat(this._currentShape || []).forEach(annotation => {
       if (annotation.kind === 'arrow') {
 
         let p1 = annotation.geometry.p1;
         let p2 = annotation.geometry.p2;
 
-        this.ctx.beginPath();
-        this.ctx.moveTo(p1.x, p1.y);
+        this._ctx.beginPath();
+        this._ctx.moveTo(p1.x, p1.y);
         var vector = {
           dx: p2.x - p1.x,
           dy: p2.y - p1.y
         }
         var length = Math.sqrt(Math.pow(vector.dx, 2) + Math.pow(vector.dy, 2));
         var percent = (length - 20.0) / length;
-        this.ctx.lineTo(p1.x + (vector.dx * percent), p1.y + (vector.dy * percent));
-        this.ctx.stroke();
+        this._ctx.lineTo(p1.x + (vector.dx * percent), p1.y + (vector.dy * percent));
+        this._ctx.stroke();
 
         var radians = Math.atan((p2.y - p1.y) / (p2.x - p1.x));
         radians += ((p2.x > p1.x) ? 90 : -90) * Math.PI / 180;
 
-        this.ctx.save();
-        this.ctx.beginPath();
-        this.ctx.translate(p2.x, p2.y);
-        this.ctx.rotate(radians);
-        this.ctx.moveTo(0, 0);
-        this.ctx.lineTo(15, 60);
-        this.ctx.lineTo(-15, 60);
-        this.ctx.closePath();
-        this.ctx.restore();
-        this.ctx.fill();
+        this._ctx.save();
+        this._ctx.beginPath();
+        this._ctx.translate(p2.x, p2.y);
+        this._ctx.rotate(radians);
+        this._ctx.moveTo(0, 0);
+        this._ctx.lineTo(15, 60);
+        this._ctx.lineTo(-15, 60);
+        this._ctx.closePath();
+        this._ctx.restore();
+        this._ctx.fill();
       }
       else if (annotation.kind === 'line') {
         let p1 = annotation.geometry.p1;
         let p2 = annotation.geometry.p2;
-        this.ctx.beginPath();
-        this.ctx.moveTo(p1.x, p1.y);
-        this.ctx.lineTo(p2.x, p2.y);
-        this.ctx.stroke();
+        this._ctx.beginPath();
+        this._ctx.moveTo(p1.x, p1.y);
+        this._ctx.lineTo(p2.x, p2.y);
+        this._ctx.stroke();
       }
       else if (annotation.kind === 'circle') {
-        this.ctx.beginPath();
+        this._ctx.beginPath();
         var g = annotation.geometry;
-        this.ctx.arc(g.center.x, g.center.y, g.radius, 0, 2 * Math.PI);
-        this.ctx.stroke();
+        this._ctx.arc(g.center.x, g.center.y, g.radius, 0, 2 * Math.PI);
+        this._ctx.stroke();
       }
       else if (annotation.kind === 'rectangle') {
-        this.ctx.beginPath();
+        this._ctx.beginPath();
         let p1 = annotation.geometry.p1;
         let p2 = annotation.geometry.p2;
-        this.ctx.strokeRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
-        this.ctx.stroke();
+        this._ctx.strokeRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
+        this._ctx.stroke();
       }
     });
   }
 
-  clampToBounds() {
-    var maxWidth = this.width * this.transform.a;
-    var maxHeight = this.height * this.transform.d;
-    this.transform.e = this.clamp(this.transform.e, -(maxWidth - this.canvas.width), 0);
-    this.transform.f = this.clamp(this.transform.f, -(maxHeight - this.canvas.height), 0);
+  _clampToBounds() {
+    var maxWidth = this.width * this._transform.a;
+    var maxHeight = this.height * this._transform.d;
+    this._transform.e = Flask.clamp(this._transform.e, -(maxWidth - this._canvas.width), 0);
+    this._transform.f = Flask.clamp(this._transform.f, -(maxHeight - this._canvas.height), 0);
   }
 
-  clamp(value, min, max) {
+  static clamp(value, min, max) {
     return Math.max(Math.min(value, max), min);
   }
 
-  eventPoint(e) {
-    var x = e.offsetX || (e.pageX - this.canvas.offsetLeft);
-    var y = e.offsetY || (e.pageY - this.canvas.offsetTop);
-    return this.transformedPoint(x, y);
+  _eventPoint(e) {
+    var x = e.offsetX || (e.pageX - this._canvas.offsetLeft);
+    var y = e.offsetY || (e.pageY - this._canvas.offsetTop);
+    return this._transformedPoint(x, y);
   }
 
-  tileImage(zoom, x, y) {
+  _tileImage(zoom, x, y) {
     var url = `http://useredline-api.s3.amazonaws.com/development/tiles/168d136e60b14850d7a671e8/tile_${zoom}_${x}x${y}.jpg`;
-    var image = this.tileCache[url];
+    var image = this._tileCache[url];
     if (!image) {
       image = new Image;
       image.src = url;
       image.onload = () => {
-        this.redraw();
+        this._redraw();
       };
-      this.tileCache[url] = image;
+      this._tileCache[url] = image;
     }
     return image;
   }
 
-  intersectRect(r1, r2) {
+  _intersectRect(r1, r2) {
     r1 = {
       left: r1.x,
       right: r1.x + r1.width,
